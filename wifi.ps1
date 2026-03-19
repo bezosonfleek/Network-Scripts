a161437072dc87682b675bbe1dd989a1d8967c1d
Geoffrey Sakora <162801736+bezosonfleek@users.noreply.github.com>
2026-03-19 15:31:33 +0300

    Update wifi.ps1

--- wifi.ps1
+++ wifi.ps1
@@ -1,2 +1,5 @@
 #Checks password for a network called X2X
 netsh wlan show profile name="X2X" key=clear | Out-String -Stream | Select-String -Pattern "Key Content"
+
+#alternatively
+netsh wlan show profile name="X2X" key=clear  | Select-String -Pattern "Key Content"
